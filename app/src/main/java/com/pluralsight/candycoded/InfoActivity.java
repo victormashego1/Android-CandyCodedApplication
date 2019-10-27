1cb15de9abb92cb104f50ac39985ee549be16e35
victormashego1 <mashegove@gmail.com>
2019-10-27 02:39:52 +0200

    Completed module 1

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -27,6 +27,8 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
 
+
+
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
